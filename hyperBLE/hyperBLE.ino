aacceb0049d556c9d3c7348b68d073d7ab79c691
ManuelBetancurt <juanman234@gmail.com>
2021-02-10 21:14:11 +1100

    rudimento update calculate freq. BLE update, acknowledge command

--- hyperBLE/hyperBLE.ino
+++ hyperBLE/hyperBLE.ino
@@ -6,45 +6,31 @@
 // #f:340 ... FREQ to 340 
 
 bool lightsON = false;
-int freq = 220;
+float freq = 17;
+int ledPin = 4;
+
 
 void setup() {
-  Serial.begin(115200);               //initial the Serial
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);                
+  pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  while (Serial.available() > 0 ) {
-    String str = Serial.readString();
-    if (str.indexOf("#1") > -1) {
-      lightsON = true;
-    }
 
-    if (str.indexOf("#0") > -1) {
-      lightsON = false;
-    }
+float periodo() {
+//  float peri = 1/freq;
+//  float halfer = peri/2;
+//  float millisec = halfer*1000;
+//  return millisec;
 
-    if (str.indexOf("#f") > -1) {
-      String xval = getValue(str, ':', 1);
-      //      xval = "x:"+xval;
-      //      const char* s;
-      //        s= xval.c_str();
-      //       Serial.write(s);
-      freq = xval.toInt();
-    }
-  }
-
-  if (lightsON) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(freq);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(freq);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-
+  return ((1/freq)/2)*1000;
 }
 
+float blinkus(){
+  digitalWrite(ledPin, HIGH);    
+  delay(periodo());                       
+  digitalWrite(ledPin, LOW);     
+  delay(periodo());   
+}
 
 String getValue(String data, char separator, int index)
 {
@@ -60,4 +46,41 @@ String getValue(String data, char separator, int index)
     }
   }
   return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void loop() {
+  
+  while (Serial.available() > 0 ) {
+
+    String str = Serial.readString();
+
+    
+    if (str.indexOf("#1") > -1) {
+      lightsON = true;
+      Serial.write("lightsON");
+    }
+
+    if (str.indexOf("#0") > -1) {
+      lightsON = false;
+      Serial.write("lightsOFF");
+    }
+
+    if (str.indexOf("#f") > -1) {
+      String xval = getValue(str, ':', 1);
+      //      xval = "x:"+xval;
+      //      const char* s;
+      //        s= xval.c_str();
+      //       Serial.write(s);
+      freq = xval.toInt();
+      Serial.write("freq set");
+    }
+
+  }
+
+  if (lightsON) {
+      blinkus();
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+
 }
